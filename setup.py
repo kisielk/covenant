2ee0f9904cad50ffff49bc71a046929c7b613b77
Kamil Kisiel <kamil@kamilkisiel.net>
2012-03-09 18:47:39 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='covenant',
+      version='0.1.0',
+      description='Contracts for Python',
+      author='Kamil Kisiel',
+      author_email='kamil@kamilkisiel.net',
+      url='http://pypi.python.org/pypi/covenant',
+      license="BSD License",
+      packages=["covenant"],
+      keywords="contract",
+      platforms=["All"],
+      install_requires=["decorator"],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities'],
+     )
