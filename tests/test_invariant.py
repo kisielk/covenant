c775035d0f2fa6b12aa786e911761a1f077faad6
Marcin Bielak <marcin.bieli@gmail.com>
2025-02-23 19:52:39 +0100

    Update test_invariant.py

--- tests/test_invariant.py
+++ tests/test_invariant.py
@@ -1,29 +1,26 @@
 import unittest
 from covenant.invariant import *
+from covenant.exceptions import *
 
-
-@unittest.skip("Not implemented yet")
 class InvariantTests(unittest.TestCase):
     def test_invariant(self):
-        self.skip()
-
-        @invariant("self.foo >= 0")
+        @invariant(lambda self: self.foo >= 0)
         class Foo(object):
+            foo = 0
+
             def __init__(self):
                 self.foo = 0
 
             def add(self, num):
                 self.foo += num
+
         f = Foo()
         f.add(5)
+
         self.assertEqual(f.foo, 5)
-        with self.assertRaises(InvariantViolation):
-            f.add(-10)
-        self.assertEqual(f.foo, 5)
-        f.foo = -5
-        with self.assertRaises(InvariantViolation):
-            f.add(100)
-        self.assertEqual(f.foo, -5)
+
+        with self.assertRaises(InvariantViolationError):
+            f.add(-6)
 
 
 if __name__ == "__main__":
